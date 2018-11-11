eb5a0d3147d775128f635d674da33e9a0cb2582f
Joss Cousins <josscousins@outlook.com>
2018-11-11 15:47:57 +0000

    refactored variable 'scale' to 'SCALE' to suit naming convention of type final

--- Rain/src/com/josscousins/rain/Game.java
+++ Rain/src/com/josscousins/rain/Game.java
@@ -5,6 +5,6 @@ public class Game {
     //GameWindow variable
     public static final int WIDTH = 300;
     public static final int HEIGHT = (WIDTH/ 16) * 9;
-    public static final int scale = 3;
+    public static final int SCALE = 3;
 
 }
